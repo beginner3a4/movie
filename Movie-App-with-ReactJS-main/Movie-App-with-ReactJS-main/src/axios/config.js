31817cdb70933ef1513d933f7c245368c58c0559
BHUSHAN YADAV <132989870+beginner3a4@users.noreply.github.com>
2025-01-04 19:00:01 +0530

    Delete Movie-App-with-ReactJS-main/Movie-App-with-ReactJS-main directory

--- Movie-App-with-ReactJS-main/Movie-App-with-ReactJS-main/src/axios/config.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosCreate = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  params: {
-    api_key: process.env.REACT_APP_API_KEY,
-  },
-});
-export default axiosCreate;
